dd58b1e200ef31f6b1cd2cd4c5cbf4f928723760
Eden Lumbroso <eden.l@fmr.ai>
2025-07-13 22:55:02 +0300

    removing unused files

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Layout from './app/Layout.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>,
-)
